86d02fd168193a776b0345392a142704e88b4de9
petadox <jesus.gimenez@est.fib.upc.edu>
2019-07-11 20:21:04 +0200

    OptionsScene added, and transitions between MainScene and OptionsScene

---
+++ PerritoGame/Assets/Scripts/Buttons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
